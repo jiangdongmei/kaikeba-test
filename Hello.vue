dc032abeeb450cf1bdbde0aa7d1a4d1149f72ee0
jiangdongmei <1341146570@qq.com>
2020-12-16 17:30:50 +0800

    animate

---
+++ Hello.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div v-for="item in items" :key="item.id">
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
